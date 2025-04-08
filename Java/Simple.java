b6e5b62cf6884b1031f0ffca33c026bb1e21bc35
sahanayaligar12 <yaligarsahana5@gmail.com>
2025-04-08 09:45:25 +0530

    saved

---
+++ Java/Simple.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Simple {
+
+    public static void main(String [] args){
+        System.out.println("Main");
+    }
+}
+
+
+
---
+++ RemoteRepo1/README.md
@@ -0,0 +1,3 @@
+0# RemoteRepo1
+This is a devops lab
+First program
